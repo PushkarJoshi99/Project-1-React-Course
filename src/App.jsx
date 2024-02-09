827e7b4f6b4cea59bd6ea9c4accae615c8af649c
PushkarJoshi99 <jpushkar.2001@gmail.com>
2024-02-10 00:28:41 +0530

    55 done

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,23 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-	const [selectedTopic, setSelectedTopic] = useState('components');
+	const [selectedTopic, setSelectedTopic] = useState();
 
 	function handleSelect(selectedButton) {
 		setSelectedTopic(selectedButton);
-		console.log(selectedTopic);
 	}
-
+	let tabContent = <p>Please select a topic</p>;
+	if (selectedTopic) {
+		tabContent = (
+			<div id="tab-content">
+				<h3>{EXAMPLES[selectedTopic].title}</h3>
+				<p>{EXAMPLES[selectedTopic].description}</p>
+				<pre>
+					<code>{EXAMPLES[selectedTopic].code}</code>
+				</pre>
+			</div>
+		);
+	}
 
 	return (
 		<div>
@@ -38,13 +48,7 @@ function App() {
 						<TabButton onClick={() => handleSelect("props")}>props</TabButton>
 						<TabButton onClick={() => handleSelect("state")}>state</TabButton>
 					</menu>
-					<div id="tab-content">
-						<h3>{EXAMPLES[selectedTopic].title}</h3>
-						<p>{EXAMPLES[selectedTopic].description}</p>
-						<pre>
-							<code>{EXAMPLES[selectedTopic].code}</code>
-						</pre>
-					</div>
+					{tabContent}
 				</section>
 			</main>
 		</div>
